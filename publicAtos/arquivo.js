adf3f6308160f36618bd4bf3d5b4e27c0945a449
AguiarD <douglasv.aguiar@gmail.com>
2021-11-09 22:59:51 -0300

    Aula 09/11/2021 - jQuery Introdução

--- publicAtos/arquivo.js
+++ publicAtos/arquivo.js
@@ -142,7 +142,7 @@
 
 // }
 
-// Aula 08/11/2021
+// **************************************************************************   Aula 08/11/2021
 
 function mudarCor(cor) {
   document.body.style.backgroundColor = cor;
@@ -151,19 +151,23 @@ function mudarCor(cor) {
 function esconderTexto() {
   var par = document.getElementById("paragrafo");
 
-  //oculta o MediaElementAudioSourceNode.
-  // if (par.style.visibility == "hidden") {
-  //   par.style.visibility = "visible";
-  // } else {
-  //   par.style.visibility = "hidden";
-  // }
-
-  //some o elemento:
   if (par.style.display == "none") {
     par.style.display = "block";
   } else {
     par.style.display = "none";
   }
+}
+
+function removerTexto() {
+  var par = document.getElementById("paragrafo");
+
+  if (par.style.visibility == "hidden") {
+    par.style.visibility = "visible";
+  } else {
+    par.style.visibility = "hidden";
+  }
+
+  //  some o elemento:
 }
 
 function verificaChecks() {
@@ -212,3 +216,84 @@ function tamanhoMenos() {
   tamanho--;
   document.getElementById("paragrafoCor").style.fontSize = tamanho + "px";
 }
+
+function maiusculo() {
+  var txtOriginal = document.getElementById("paragrafoCor");
+  txtOriginal.innerText = txtOriginal.innerText.toUpperCase();
+}
+
+function minusculo() {
+  document.getElementById("paragrafoCor").innerHTML =
+    document.getElementById("paragrafoCor").innerHTML.toLocaleLowerCase();
+}
+
+function validar() {
+  var nome = document.getElementById("nome").value;
+  var senha1 = document.getElementById("senha1").value;
+  var senha2 = document.getElementById("senha2").value;
+
+  if (nome != "" && senha1 != "" && senha2 != "") {
+    if (senha1 == senha2) {
+      if (senha1.length > 5 && senha1.length < 11) {
+        alert("Salvo com sucesso!");
+      } else {
+        alert("A senha precisa ter mínimo de 5 caracteres!");
+      }
+    } else {
+      alert("Senhas precisam ser iguais!");
+    }
+  } else {
+    alert("Favor preencher os campos!");
+  }
+
+}
+
+function alteraVisibilidade(tipop) {
+  if (tipop == "fisica") {
+    document.getElementById("pessoaFisica").style.display = "block";
+    document.getElementById("pessoaJuridica").style.display = "none";
+  } else {
+    document.getElementById("pessoaFisica").style.display = "none";
+    document.getElementById("pessoaJuridica").style.display = "block";
+  }
+}
+
+function mascara(m, t, e) {
+  var cursor = t.selectionStart;
+  var texto = t.value;
+  texto = texto.replace(/\D/g, '');
+  var l = texto.length;
+  var lm = m.length;
+  if (window.event) {
+    id = e.keyCode;
+  } else if (e.which) {
+    id = e.which;
+  }
+  cursorfixo = false;
+  if (cursor < l) cursorfixo = true;
+  var livre = false;
+  if (id == 16 || id == 19 || (id >= 33 && id <= 40)) livre = true;
+  ii = 0;
+  mm = 0;
+  if (!livre) {
+    if (id != 8) {
+      t.value = "";
+      j = 0;
+      for (i = 0; i < lm; i++) {
+        if (m.substr(i, 1) == "#") {
+          t.value += texto.substr(j, 1);
+          j++;
+        } else if (m.substr(i, 1) != "#") {
+          t.value += m.substr(i, 1);
+        }
+        if (id != 8 && !cursorfixo) cursor++;
+        if ((j) == l + 1) break;
+      }
+    }
+  }
+  if (cursorfixo && !livre) cursor--;
+  t.setSelectionRange(cursor, cursor);
+}
+
+
+// **************************************************************************   Aula 09/11/2021
