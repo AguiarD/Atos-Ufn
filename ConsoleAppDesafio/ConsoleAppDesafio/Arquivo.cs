5a743e3ea2a2edbd8009d4ef0ea95497d14a08b0
AguiarD <douglasv.aguiar@gmail.com>
2021-12-09 18:48:38 -0300

    Desafio aula 07/12/2021

---
+++ ConsoleAppDesafio/ConsoleAppDesafio/Arquivo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDesafio
+{
+    class Arquivo
+    {        
+        StreamReader sr;
+        
+        public void lerArquivo()
+        {
+            string linha, tipo;
+            sr = new StreamReader("C:\\Users\\Cliente\\Pictures\\teste\\desafio1.txt");
+            linha = sr.ReadLine();
+            while (linha != null)
+            {
+                linha = sr.ReadLine();
+                //Console.WriteLine(linha);
+
+                //tipo = linha.Substring(0, 2);
+                //if (tipo == "Z")
+                //{
+                    string[] pessoas = linha.Split('-');
+
+                    foreach (var pessoa in pessoas)
+                    {
+                        if (pessoas[0] == "Z")
+                        {
+                            Console.WriteLine(pessoas[1]);
+                        }
+                    }
+                        Console.WriteLine();
+                //}
+            }
+            sr.Close();
+
+        }
+
+        /*public void gravaMensagem(string mensagem)
+        {
+            sw.WriteLine(mensagem);
+        }
+
+        public void fechaArquivo()
+        {
+            sw.Close();
+        }*/
+
+
+    }
+}
---
+++ ConsoleAppDesafio/ConsoleAppDesafio/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDesafio
+{
+    class Pessoa
+    {
+        protected string nome;
+        protected string telefone;
+        protected string cidade;
+        protected string rg;
+        protected string cpf;
+    }
+}
