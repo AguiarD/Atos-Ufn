3776f0374d7dcc899b2fe32b2338540bc7141c97
AguiarD <douglasv.aguiar@gmail.com>
2022-01-05 23:34:23 -0300

    Atos/UFN_aula_REST_API_com_Autenticacao-Editei_o_arquivo-WebApplicationAulaRestAPI

--- WebApplicationAulaRestAPI/WebApplicationAulaRestAPI/Controllers/PessoaController.cs
+++ WebApplicationAulaRestAPI/WebApplicationAulaRestAPI/Controllers/PessoaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplicationAulaRestAPI.Data;
@@ -6,11 +7,19 @@ using WebApplicationAulaRestAPI.Models;
 
 namespace WebApplicationAulaRestAPI.Controllers
 {
+    [Authorize]
     //[Route("api/[controller]")]
     [Route("api")]  // url base (url base-> http://localhost:5852; controller-> api/pessoas; endpoint-> todas)
     [ApiController]
     public class PessoaController : ControllerBase
     {
+        private readonly IJWTAuthenticationManager jwtAuthenticationManager;
+
+        public PessoaController(IJWTAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+
         //Contexto contexto; // Gerar uma instancia de contexto mas como criei em ... não precisa mais
 
         [HttpGet]
@@ -137,6 +146,18 @@ namespace WebApplicationAulaRestAPI.Controllers
             }
         }
 
+        [AllowAnonymous]  //este metodo nao precisa de autenticacao, essa notation anula o [autorize]
+        [HttpPost("autenticar")]
+        public IActionResult Authenticate([FromBody] Usuario user)
+        {
+            var token = jwtAuthenticationManager.Authenticate(user.Username, user.Passoword);
 
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(token);
+        }
     }
 }
---
+++ WebApplicationAulaRestAPI/WebApplicationAulaRestAPI/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplicationAulaRestAPI.Models
+{
+    public class Usuario
+    {
+        public string Username { get; set; }
+
+        public string Passoword { get; set; }
+    }
+}
